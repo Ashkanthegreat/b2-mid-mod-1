1b49719553acec51798e614e24f6b7cff7f6f396
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-17 09:34:05 -0600

    create mechanics, amusement parks, and rides migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200717152209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "amusement_parks", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_of_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
+    t.bigint "amusement_park_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["amusement_park_id"], name: "index_rides_on_amusement_park_id"
+  end
+
+  add_foreign_key "rides", "amusement_parks"
 end
---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "mechanics index page" do
+  it "as a visitor when I visit mechanics index page, I see each mechanic and years of experience" do
+
+  end
+end  
