757180cc83c9058b5840e0c7c975c5f716ee7118
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-17 09:52:34 -0600

    Finish US1

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe "mechanics index page" do
   it "as a visitor when I visit mechanics index page, I see each mechanic and years of experience" do
 
+    mech1 = Mechanic.create(name: "Joe", years_of_experience: 12)
+    mech2 = Mechanic.create(name: "Jim", years_of_experience: 16)
+
+    visit '/mechanics'
+
+    expect(page).to have_content(mech1.name)
+    expect(page).to have_content(mech1.years_of_experience)
+    expect(page).to have_content(mech2.name)
+    expect(page).to have_content(mech2.years_of_experience)
   end
-end  
+end
